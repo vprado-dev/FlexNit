49b9f3b245a1a1fa804a560f4b3360ca7b44f01e
rodcoffani <rodrigo.coffani@gmail.com>
2019-11-22 13:21:35 -0300

    Arrumando o script SQL

--- Flexnit_postgres_create.sql
+++ Flexnit_postgres_create.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS "filmes_assistidos" CASCADE;
 DROP TABLE IF EXISTS "admin" CASCADE;
 
 CREATE TABLE "filmes" (
-	"id_filme" INTEGER NOT NULL,
+	"id_filme" SERIAL NOT NULL,
 	"titulo_filme" varchar(30) NOT NULL,
 	"descricao_filme" varchar(255) NOT NULL,
 	"ano_filme" integer NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE "filmes" (
 	CONSTRAINT "filmes_pk" PRIMARY KEY ("id_filme")
 );
 CREATE TABLE "genero" (
-	"id_genero" INTEGER NOT NULL,
+	"id_genero" SERIAL NOT NULL,
 	"nome_gen" varchar(100) NOT NULL,
 	CONSTRAINT "gênero_pk" PRIMARY KEY ("id_genero")
 
@@ -23,8 +23,8 @@ CREATE TABLE "genero" (
 CREATE TABLE "cliente" (
 	"id_cliente" SERIAL NOT NULL UNIQUE,
 	"nome_cliente" varchar(100) NOT NULL,
-	"email_cliente" varchar(150) NOT NULL,
-	"username_cliente" varchar(100) NOT NULL,
+	"email_cliente" varchar(150) NOT NULL UNIQUE,
+	"username_cliente" varchar(100) NOT NULL UNIQUE,
 	"estado_cliente" varchar(50) NOT NULL,
 	"senha_cliente" varchar(200) NOT NULL,
 	"excluido" BOOLEAN NOT NULL DEFAULT 'false',
@@ -33,16 +33,16 @@ CREATE TABLE "cliente" (
 CREATE TABLE "admin"(
 	"id_admin" SERIAL NOT NULL UNIQUE,
 	"nome_admin" CHARACTER VARYING(100) NOT NULL,
-	"user_admin" CHARACTER VARYING(100)NOT NULL,
-	"email_admin" CHARACTER VARYING(100) NOT NULL,
+	"user_admin" CHARACTER VARYING(100)NOT NULL UNIQUE,
+	"email_admin" CHARACTER VARYING(100) NOT NULL UNIQUE,
 	"senha_admin" CHARACTER VARYING(100)NOT NULL,
 	CONSTRAINT "admin_pk" PRIMARY KEY ("id_admin")
 	
 );
 CREATE TABLE "filmes_assistidos" (
-	"id_assistido" INTEGER NOT NULL UNIQUE,
-	"id_cliente" INTEGER NOT NULL UNIQUE,
-	"id_filme" INTEGER NOT NULL UNIQUE,
+	"id_assistido" SERIAL NOT NULL,
+	"id_cliente" INTEGER NOT NULL,
+	"id_filme" INTEGER NOT NULL,
 	CONSTRAINT "filmes_assistidos_pk" PRIMARY KEY ("id_assistido")
 );
 ALTER TABLE "filmes" ADD CONSTRAINT "filmes_fk0" FOREIGN KEY ("id_genero") REFERENCES "genero"("id_genero");
@@ -52,4 +52,3 @@ ALTER TABLE "filmes" ADD CONSTRAINT "filmes_fk1" FOREIGN KEY ("id_cliente") REFE
 
 ALTER TABLE "filmes_assistidos" ADD CONSTRAINT "filmes_assistidos_fk0" FOREIGN KEY ("id_cliente") REFERENCES "cliente"("id_cliente");
 ALTER TABLE "filmes_assistidos" ADD CONSTRAINT "filmes_assistidos_fk1" FOREIGN KEY ("id_filme") REFERENCES "filmes"("id_filme");
-
